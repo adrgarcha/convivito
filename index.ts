80ac69e596f8eb669b5bb76b404e823b15e7d021
adrgarcha <adrgarcha@alum.us.es>
2025-01-09 12:07:37 +0100

    feat: implement message sending functionality

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hi Convivito!");
+import { ACCESS_TOKEN, API, BOT_ID, TEST_RECIPIENT } from './lib/constants';
+
+const sendMessageText = async (message: string) => {
+   try {
+      const response = await fetch(`${API}/${BOT_ID}/messages`, {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+         },
+         body: JSON.stringify({
+            messaging_product: 'whatsapp',
+            to: TEST_RECIPIENT,
+            type: 'text',
+            text: {
+               body: message,
+            },
+         }),
+      });
+
+      if (response.ok) {
+         console.log('¡Mensaje enviado satisfactoriamente!');
+      } else {
+         console.error(`Error enviando el mensaje: ${response.statusText}`);
+      }
+   } catch (error) {
+      throw new Error(`Error enviando el mensaje: ${error}`);
+   }
+};
+
+sendMessageText('¡Hola desde Convivito!');
---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const { BOT_ID, ACCESS_TOKEN, TEST_RECIPIENT } = process.env;
+
+export const API = 'https://graph.facebook.com/v21.0';
